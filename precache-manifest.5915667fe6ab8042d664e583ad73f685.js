24dd985d4f278a371aa314c2001081800bf2e5aa
Riyas Mohammad <riyasmohammad@Riyass-MBP.fios-router.home>
2020-06-13 22:40:32 -0400

    My protfolio changes step 1

---
+++ precache-manifest.5915667fe6ab8042d664e583ad73f685.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "549619ff95ff40a1fcfc57b0b54189ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3794f5be0a6a55f58db",
+    "url": "/static/css/main.cd94acd4.chunk.css"
+  },
+  {
+    "revision": "9ead587e92a1fbfe0141",
+    "url": "/static/js/2.339271d3.chunk.js"
+  },
+  {
+    "revision": "ddb934a41eb6bf13706c65fe3758fb10",
+    "url": "/static/js/2.339271d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3794f5be0a6a55f58db",
+    "url": "/static/js/main.c58bd514.chunk.js"
+  },
+  {
+    "revision": "f6fac08a1749a9dbaa22",
+    "url": "/static/js/runtime-main.a106779c.js"
+  },
+  {
+    "revision": "e785bc941ba6b6a6d3d420c73d7978e6",
+    "url": "/static/media/pic.e785bc94.png"
+  }
+]);
